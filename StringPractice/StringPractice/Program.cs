7a8b4bb0fa7f2be9b9661890fc158dfb62d45605
MaverickSamurai <mirsamirba@code.edu.az>
2022-06-21 17:42:45 +0400

    String-Practice

---
+++ StringPractice/StringPractice/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //int b = a;
+            //Console.WriteLine(a+ "-" + b);
+
+            //int [] arr1 = { 1, 2, 3, 4, 5, 7 };
+            //int[] arr2 = arr1;
+            //arr2[0] = 100;
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+
+
+            //int a = 6;
+            //CheckNum(a);
+            //Console.WriteLine(a);
+
+
+
+            //int a = 6;
+            //CheckNum(ref a);
+            //Console.WriteLine(a);
+
+
+
+            //int a = 6;
+            //CheckNum(out a);
+            //Console.WriteLine(a);
+
+
+
+
+            //string name1 = "Orxan";
+            //string name2 = name1;
+            //name2 = "Emil";
+            //name1 = "Huseyn";
+            //Console.WriteLine(name1 + "-"+name2);
+
+
+
+            //StringBuilder name = new StringBuilder();
+
+            //name.Append("Or");
+            //name.Append("xan");
+            //Console.WriteLine(name);
+
+
+            //string name = "Mirsamir";
+            //name = string.Empty;
+            //Console.WriteLine(name);
+
+
+            //string name = "Mirsamir";
+            //name = string.Empty;
+
+            //if (string.IsNullOrEmpty(name)) 
+            //{ 
+            //    Console.WriteLine("Dogrudur");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sehvdir");
+            //}
+
+            //string name = "Mirsamir";
+
+            //if (name.StartsWith("M"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+
+            //string name = "Mirsamir";
+            //Console.WriteLine(name.Length);
+
+
+            //string name = "Mirsamir";
+            //Console.WriteLine(name.IndexOf("s"));
+
+
+
+            //string name = "Mirsamir";
+
+            //Console.WriteLine(name.Replace("m", "b"));
+
+
+            //string name = "Mirsamir";
+            //Console.WriteLine(name.Substring(3));
+
+
+            //string name = "    Mirsamir";
+            //Console.WriteLine(name.Trim());
+
+
+            //string name = "Mirsamir";
+            //var result = name.Split(" ");
+            //Console.WriteLine(result);
+
+
+            //string name = "Mirsamir dersde yatir";
+            //var result = name.Split(" ");
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //string sentence = "Hello P130 , How are you? , bye bye";
+            //var result = sentence.Split(",");
+            //foreach (var item in sentence)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //string name = "Mirsamir";
+            //Console.WriteLine(name.ToLower());
+            //if (name.ToLower().StartsWith("m"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+
+            //string name = "Mirsamir";
+            //Console.WriteLine(name.ToUpper());
+            //if (name.ToUpper().StartsWith("M"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+
+
+        }
+        //static void CheckNum(int num)
+        //    {
+        //        num += 10;
+        //        Console.WriteLine($"Number : {num}");
+        //    }
+
+        //static void CheckNum(ref int num)
+        //{
+        //    num += 10;
+        //    Console.WriteLine($"Number : {num}");
+        //}
+
+
+        //static void CheckNum(out int num)
+        //{
+        //    num = 100;
+        //    Console.WriteLine($"Number : {num}");
+        //}
+    }
+}
